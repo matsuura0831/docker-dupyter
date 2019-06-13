39bda7fa9d614741bfb7a7e57eb2f2ca1d0aca4b
matsuura <matsuura0831@gmail.com>
2019-06-13 20:08:40 +0900

    first commit.

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FLAG_AUTH=${AUTH:=1}
+
+CONF=$HOME/.jupyter/jupyter_notebook_config.py
+
+if [ "$FLAG_AUTH" == "0" ]; then
+  echo "c.NotebookApp.token = ''" >> $CONF
+  echo "c.NotebookApp.password = ''" >> $CONF
+fi
+
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+
+xvfb-run -s "-screen 0 1400x900x24" \
+  jupyter notebook --ip='*' --notebook-dir=/workspace --no-browser --allow-root
